aa84a330522d3d62b508664389b463db481e0735
singhabhishek22957 <singhabhishek22957@gmail.com>
2026-09-13 09:13:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const app = express()
+
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "hello world",
+        success: true,
+        statusCode:200
+    })
+})
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        message: "Everything is fine",
+        success: true,
+        statusCode:200
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+    console.log(`Visit: http://localhost:${port}`)
+})
